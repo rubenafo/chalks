6a3fc81434471c2ee8f84322fdc900a9c6f9bcc8
figurebelow <ruben@figurebelow.com>
2018-08-25 11:47:12 +0100

    Accept sketch as second parameter

--- server.js
+++ server.js
@@ -7,45 +7,43 @@ var path = require('path');
 // Using the filesystem module
 var fs = require('fs');
 
-var server = http.createServer(handleRequest);
-server.listen(8080);
-
-console.log('Server started on port 8080');
-
 function handleRequest(req, res) {
-  // What did we request?
   var pathname = req.url;
-
-  // If blank let's ask for index.html
   if (pathname == '/') {
     pathname = '/index.html';
   }
-
-  // Ok what's our file extension
   var ext = path.extname(pathname);
-
-  // Map extension to file type
   var typeExt = {
     '.html': 'text/html',
     '.js':   'text/javascript',
     '.css':  'text/css'
   };
-  // What is it?  Default to plain text
-
   var contentType = typeExt[ext] || 'text/plain';
-
-  // User file system module
-  fs.readFile(__dirname + pathname,
-    // Callback function for reading
+  fs.readFile(__dirname + pathname, "utf-8",
+    
     function (err, data) {
-      // if there is an error
+      let sketchFile = process.argv[2]
+      if (pathname === "/index.html") {
+        if (sketchFile) {
+          data = data.replace("sketch.js", sketchFile)
+          console.log("Rendering", sketchFile, "...")
+        }
+        else {
+          console.log ("Rendering default sketch.js ...")
+        }
+      }
       if (err) {
         res.writeHead(500);
         return res.end('Error loading ' + pathname);
+        server.close()
       }
-      // Otherwise, send the data, the contents of the file
       res.writeHead(200,{ 'Content-Type': contentType });
       res.end(data);
-    }
+    },
   );
 }
+
+var server = http.createServer(handleRequest);
+server.listen(8080);
+
+console.log('Server started on port 8080');
