3958a724e9cab1e0541227cf8c018bdf393679a7
figurebelow <ruben@figurebelow.com>
2018-10-21 08:12:28 +0100

    Integrate with p5js. Move canvas load to p5js. Remove Rnd module

--- src/chalks.js
+++ src/chalks.js
@@ -1,8 +1,6 @@
 
 let Layout = require("./Layout")
-//let Rnd = require ("./Rnd").Rnd
 let Path = require("./Path")
-let Rnd = require("./Rnd").Rnd
 let Parametrics = require("./Parametrics")
 let wcag = require("wcag-contrast")
 
@@ -13,22 +11,17 @@ function debug(str) {
 
 class Scene {
   constructor(params, style = {}) {
+    this.start = Date.now()
     this.scale = params.scale || 1
     this.width = params.width * this.scale || 1000
     this.height = params.height * this.scale || 1000
-    this.canvas = document.getElementById('canvas');
-    canvas.width = this.width
-    canvas.height = this.height
-    this.ctx = this.canvas.getContext('2d')
-    this.start = Date.now()
-    if (params.seed) {
-      debug("using seed=" + params.seed)
-      this.rnd = new Rnd(params.seed)
-    } else {
-      let seed = (Math.random() * 10000).toString().substr(5, 8)
-      debug("using seed: " + seed)
-      this.rnd = new Rnd(seed)
-    }
+    this.p5canvas = createCanvas(this.width,this.height)
+
+    this.canvas = document.getElementById('defaultCanvas0');
+    this.ctx = this.canvas.getContext("2d")
+    this.seed = params.seed ? params.seed : (Math.random() * 10000).toString().substr(5, 8)
+    debug("using seed=" + params.seed)
+    randomSeed(this.seed)
     this.children = []
     this.drawBackground(this.width, this.height, style)
   }
@@ -41,16 +34,6 @@ class Scene {
 
   setStyle(style) {
     this.drawBackground(this.width, this.height, style)
-  }
-
-  rint(lower, upper) {
-    return this.rnd.int(lower, upper)
-  }
-  rand(lower, upper) {
-    return this.rnd.random(lower, upper)
-  }
-  pick(list, ...elems) {
-    return this.rnd.pick(list, ...elems)
   }
 
   path(style) {
@@ -75,15 +58,16 @@ class Scene {
     return grad
   }
 
-  draw() {
-    this.children.filter(c => !c.style.hide).forEach(c => c.draw(this.scale))
+  draw(loops=1, targetFile) {
+    let i = loops
+    while (i--)
+      this.children.filter(c => !c.style.hide).forEach(c => c.draw(this.scale))
     debug("ended" + " (" + (Date.now() - this.start) / 1000 + " secs)")
+    if (targetFile) {
+      saveCanvas(this.p5canvas, targetFile, 'png');
+    }
+    noLoop()
   }
-
-  //saveTo(fileName) {
-  //  this.canvas.createPNGStream().pipe(fs.createWriteStream(fileName))
-  //    console.log("Save to " + fileName + " (" + (Date.now() - this.start) / 1000 + " secs)")
-  //}
 }
 
 module.exports = {
